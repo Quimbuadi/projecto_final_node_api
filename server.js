4b44068230210e5f679b88319ea8c3ebc7a1561f
Stranger <stranger@localhost.localdomain>
2025-07-12 02:29:44 +0100

    instalar a lib dotenv, fazendo conexao com Mysql usando dotenv e variaveis de ambiente

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 import app from './src/app.js'
+
+import connection from './src/infrastructure/database/connection.js'
+
 const PORT = 3000
 
+//fazer connection 
+connection.connect((error) => {
 
+    if(error){
+        console.log(error.message)
+    }
 
-//escutar a porta 3000
-app.listen(PORT, () => {
+    console.log('conexao realizada com sucesso!')
 
-    console.log(`Servidor rodando no endereco  http://localhost:${PORT}`)
+    //escutar a porta 3000
+    app.listen(PORT, () => {
 
+        console.log(`Servidor rodando no endereco  http://localhost:${PORT}`)
+
+    })
 })
+
